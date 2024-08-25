5aae0c7651ebdd4c8685175a2a36b5a7dfa8636c
Piyush Dubey <piyushdubey@Piyushs-MacBook-Air.local>
2024-08-25 12:23:15 +0530

    installing the necessary dependancy

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { SessionProvider } from 'next-auth/react';
 import { Navbar } from '@/components/Navbar';
 
+
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <SessionProvider>
